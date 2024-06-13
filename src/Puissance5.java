322b56caae9d1e2e97c4ece44b0414bcfcdfc57f
Emir-de-la-moquette <shanky.yt@gmail.com>
2024-06-13 10:59:03 +0200

    pas sur que ça marche mdrrrr

--- src/Puissance5.java
+++ src/Puissance5.java
@@ -16,6 +16,8 @@ public class Puissance5 extends Application {
     Pane panelCentral;
     Pane top;
     FXMLLoader loaderjeu;
+    int largeur = 7;
+    int hauteur = 6;
 
         /**
      * initialise les attributs
@@ -67,7 +69,17 @@ public class Puissance5 extends Application {
     private void jeu(){
         try {
             this.panelCentral = loaderjeu.load();
-            for(int i=0; i<largeur; )
+            HBox select_col = new HBox;
+            GridPane plateau = new GridPane;
+            for(int x=0; x<largeur; x++){
+                Button pos_col = new Button("V"); // a ajouter les trucs sur bouton
+                select_col.getchildren().add(pos_col);
+                for(int y=0; y<hauteur; y++){
+                    Circle cercle = New Circle();
+                    cercle.setColor(Color.WHITE);
+                    GridPane.add(x, y, cercle);
+                }
+            }
         } catch (Exception e) {
             this.panelCentral = new Pane();
         }
